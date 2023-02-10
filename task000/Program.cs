c1b55941d082319df7c7b7f67cdeb9470f904d3a
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 21:04:18 +0300

    Creating task000 directory

---
+++ task000/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+string answer = Console.ReadLine();
+int number = Convert.ToInt32(answer); // преобразование string в int/double;
+
+int result = number * number;
+
+Console.WriteLine(result);
+
