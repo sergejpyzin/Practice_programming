3f8fa5056ed2d009be1b9f73f445477335b6b093
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-10 21:56:49 +0300

    Creating task001 directory

---
+++ task001/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write ("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write ("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int result = numberB * numberB;
+
+if (result == numberA) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
