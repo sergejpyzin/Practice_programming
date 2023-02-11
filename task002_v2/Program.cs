e51ae11b8f831d98ac518ea4a2203cbe1f81354e
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 08:54:51 +0300

    created task002_v2 directory (swich_case)

---
+++ task002_v2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write ("Введите номер дня недели: ");
+int day = Convert.ToInt32 (Console.ReadLine ());
+
+switch (day)
+{
+    case 1:
+        Console.WriteLine ("Понедельник");
+        break;
+    case 2:
+        Console.WriteLine ("Вторник");
+        break;
+    case 3:
+        Console.WriteLine ("Среда");
+        break;
+    case 4:
+        Console.WriteLine ("Четверг");
+        break;
+    case 5:
+        Console.WriteLine ("Пятница");
+        break;
+    case 6:
+        Console.WriteLine ("Суббота");
+        break;
+    case 7:
+        Console.WriteLine ("Воскресенье");
+        break;
+
+    default:
+        Console.WriteLine ("Внимание! Будьте осторожны. Вы ввели неправельный день недели.");
+        break;
+}
+
