7fee7fa7c3837c0c42cb06d3e8a85df2342c26d8
Sergej Pyzin <sergejpyzin@yandex.ru>
2023-02-11 08:45:09 +0300

    changed task002 (massive0)

--- task002/Program.cs
+++ task002/Program.cs
@@ -1,5 +1,11 @@
 ﻿Console.Write("Введите номер дня недели: ");
-int day = Convert.ToInt32(Console.ReadLine());
+int answer = Convert.ToInt32(Console.ReadLine());
+string day = "";
 
-string [] days = {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
-if (day < )
+string [] days = new string [] {"понедельник", "вторник", "среда", "четверг", "пятница", "суббота", "воскресенье"};
+if (answer < 1 || answer > 7) {
+    Console.WriteLine("Будьте внимательными. Неправильный номер дня недели.");
+} else {
+    day = days [answer - 1];
+    Console.WriteLine (day);
+}
